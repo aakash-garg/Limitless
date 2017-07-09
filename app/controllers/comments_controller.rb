fbc003bc89bac68bebf2c50c5fade63891c23f0d
Aakash Garg <aakash.garg80@gmail.com>
2017-07-10 01:44:55 +0530

    add comment section

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,42 @@
+class CommentsController < ApplicationController
+  # before_action :set_comment, only: [:update, :destroy]
+
+
+  # POST /comments
+  # POST /comments.json
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(params[:comment].permit(:content))
+
+    redirect_to post_path(@post)
+  end
+
+  # PATCH/PUT /comments/1
+  # PATCH/PUT /comments/1.json
+  def update
+    respond_to do |format|
+      if @comment.update(comment_params)
+        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
+        format.json { render :show, status: :ok, location: @comment }
+      else
+        format.html { render :edit }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /comments/1
+  # DELETE /comments/1.json
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+
+    @comment.destroy
+    redirect_to post_path(@post)
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:content)
+    end
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,6 @@ class Post < ActiveRecord::Base
   validates :title, presence: true, length: {within: 5..50}
   validates :body, presence: true, length: {within: 10..500}
   belongs_to :user
+  has_many :comments, dependent: :destroy
   acts_as_votable
 end
