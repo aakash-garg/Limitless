4926cb11a906f8a8c72cbdaf049b17817be01a23
Aakash Garg <aakash.garg80@gmail.com>
2017-07-17 22:02:06 +0530

    add votable

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :posts do
-    resources :comments
     member do
       put "like", to: "posts#upvote"
       put "dislike", to: "posts#downvote"
+    end
+    resources :comments do
+      member do
+        put "like", to: "comments#upvote"
+        put "dislike", to: "comments#downvote"
+      end
     end
   end
 
