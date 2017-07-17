857c5649e06b8de463da2af59a67d902d311b48b
Aakash Garg <aakash.garg80@gmail.com>
2017-07-18 02:12:59 +0530

    add profile_pic_default

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170717185045) do
+ActiveRecord::Schema.define(version: 20170717201134) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "content"
@@ -54,6 +54,10 @@ ActiveRecord::Schema.define(version: 20170717185045) do
     t.string   "name"
     t.datetime "date_of_birth"
     t.boolean  "is_female",              default: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
