c5e4e41c8a98fc8faa88ba9b5bc4dd98751c8ce7
Aakash Garg <aakash.garg80@gmail.com>
2017-07-18 18:14:37 +0530

    add header styling

--- Gemfile
+++ Gemfile
@@ -31,6 +31,8 @@ gem 'acts_as_votable', '~> 0.10.0'
 
 gem "paperclip", "~> 5.0.0"
 
+gem 'normalize-rails'
+
 # end
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ActiveRecord::Base
   has_many :posts, dependent: :destroy
   has_many :comments
 
-  has_attached_file :image, styles: { medium: "300x300#", small: "100x100#" }, :default_url => ActionController::Base.helpers.asset_path("user-profile.png")
+  has_attached_file :image, styles: { medium: "300x300#", small: "100x100#", logo: "10x10#" }, :default_url => ActionController::Base.helpers.asset_path("user-profile.png")
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
