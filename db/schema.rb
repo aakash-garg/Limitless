9e7ebc693a78d2eaca88a7822266b7adf55d0677
Aakash Garg <aakash.garg80@gmail.com>
2017-08-04 21:39:18 +0530

    add style

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170719190555) do
+ActiveRecord::Schema.define(version: 20170803045302) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content",    limit: 60000
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 20170719190555) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.text     "bio"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
