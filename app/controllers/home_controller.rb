0817d124c1e42af55086d6b4770998b3727d6915
Aakash Garg <aakash.garg80@gmail.com>
2017-08-14 01:28:35 +0530

    add ajax to users

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,34 +11,45 @@ class HomeController < ApplicationController
 
   def follow
     @followee_id = params[:followee_id]
+    if current_user.can_follow @followee_id
+      FollowMapping.create(:followee_id => @followee_id, :follower_id => current_user.id)
+    end
     # byebug
     if params[:post]
-      @post=Post.find(params[:post].to_i)
+      @post = Post.find(params[:post].to_i)
+      @user = @post.user
+      respond_to do |format|
+        format.html { redirect_to :back }
+        format.js
+      end
     else
       @user = User.find(params[:followee_id])
+      respond_to do |format|
+        format.html { redirect_to :back }
+        format.js { render :action => 'follow_2' }
+      end
     end
-    if current_user.can_follow @followee_id
-    FollowMapping.create(:followee_id => @followee_id, :follower_id => current_user.id)
-    end
-    respond_to do |format|
-      format.html { redirect_to :back }
-      format.js
-    end
+
   end
 
   def unfollow
     @followee_id = params[:followee_id]
-    if params[:post]
-      @post=Post.find(params[:post].to_i)
-    else
-      @user = User.find(params[:followee_id])
-    end
     if current_user.can_unfollow @followee_id
       FollowMapping.where(:followee_id => @followee_id, :follower_id => current_user.id).first.destroy
     end
-    respond_to do |format|
-      format.html { redirect_to :back }
-      format.js
+    if params[:post]
+      @post = Post.find(params[:post].to_i)
+      @user = @post.user
+      respond_to do |format|
+        format.html { redirect_to :back }
+        format.js
+      end
+    else
+      @user = User.find(params[:followee_id])
+      respond_to do |format|
+        format.html { redirect_to :back }
+        format.js { render :action => 'unfollow_2' }
+      end
     end
   end
 
