e21c5e832cac48f7a7d8360134060da83dc74d73
Aakash Garg <aakash.garg80@gmail.com>
2017-08-20 01:17:17 +0530

    resolved ajax conflicts

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,41 +11,37 @@ class CommentsController < ApplicationController
     @comment.save
     # redirect_to post_path(@post)
     respond_to do |format|
-      format.html {redirect_to @post}
+      format.html {
+        redirect_to @post
+        flash[:success] = 'Comment posted.'
+      }
       format.js
     end
   end
 
-  # PATCH/PUT /comments/1
-  # PATCH/PUT /comments/1.json
-  def update
-    respond_to do |format|
-      if @comment.update(comment_params)
-        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
-        format.json { render :show, status: :ok, location: @comment }
-      else
-        format.html { render :edit }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
-    end
-  end
 
   # DELETE /comments/1
   # DELETE /comments/1.json
   def destroy
     @post = Post.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
-
     @comment.destroy
-    redirect_to post_path(@post)
+    respond_to do |format|
+      format.html{
+        redirect_to post_path(@post)
+        flash[:success] = 'Comment deleted.'
+      }
+      format.js
+    end
   end
 
   def upvote
     @comment = Comment.find(params[:id])
     @comment.upvote_by current_user
     respond_to do |format|
-      format.html {redirect_to :back }
-      format.json { render json: { count: @post.liked_count } }
+      format.html {
+        redirect_to :back
+      }
       format.js   { render :layout => false }
     end
   end
@@ -54,8 +50,9 @@ class CommentsController < ApplicationController
     @comment = Comment.find(params[:id])
     @comment.downvote_by current_user
     respond_to do |format|
-      format.html {redirect_to :back }
-      format.json { render json: { count: @post.liked_count } }
+      format.html {
+        redirect_to :back
+      }
       format.js   { render :layout => false }
     end
   end
