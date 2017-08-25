ed2262a6cd397e427e742292dc92a34dc2b57d2b
Aakash Garg <aakash.garg80@gmail.com>
2017-08-26 02:32:22 +0530

    before resolve infinite scroll

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def users
-    @users = User.all.order('created_at DESC').page(params[:page]).per_page(4)
+    @users = User.all.order('created_at DESC').page(params[:page]).per_page(6)
   end
 
   def profile
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  validates :title, presence: true, length: {within: 5..50}
+  validates :title, presence: true, length: {within: 5..80}
   validates :body, presence: true, length: {within: 10..50000}
   belongs_to :user
   has_many :comments, dependent: :destroy
