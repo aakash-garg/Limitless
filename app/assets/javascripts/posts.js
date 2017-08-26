abd729af1586006c98a446f751c596adce33f64b
Aakash Garg <aakash.garg80@gmail.com>
2017-08-26 13:35:38 +0530

    errors resolved

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,6 +1,6 @@
 
 $(window).scroll(function(){
-    url = $('.posts-scroll .next_page').attr('href');
+    url = $('.pagination .next_page').attr('href');
     if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50){
         // $.ajax({
         //     url: $(".pagination .next_page").attr("href"),
@@ -9,7 +9,7 @@ $(window).scroll(function(){
         //         console.log(arguments);
         //         console.log("Content was loaded");
         //     }
-        $('.posts-scroll').text('Loading more content ...')
+        $('.pagination').text('Loading more content ...')
         $.getScript(url);
         }
 
