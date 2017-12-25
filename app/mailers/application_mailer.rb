404a7e4e4975a62a516ebac4406b78f20f910cd2
Aakash Garg <aakash.garg80@gmail.com>
2017-12-25 12:26:44 +0530

    Some changes and updates

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "be.limitless@gmail.com"
+  default from: "aakashgargdtu98@gmail.com"
   layout 'mailer'
   add_template_helper EmailHelper
   add_template_helper ApplicationHelper
